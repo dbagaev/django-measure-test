1fc6056a23954476db33550f4b5909d4d32451d0
Dmytro Bagaiev <dmytro.bagaiev@gmail.com>
2015-12-31 17:38:42 +0100

    Can load and show lost of tests

--- views.py
+++ views.py
@@ -8,18 +8,6 @@ from MeasureTest.MeasureTest import *
 def index(request) :
     out = []
 
-    context = {'tests': []}
-
-    for cls in MeasuredTest.All :
-        e = cls()
-        e.TestName = e.__class__.__name__
-        e.test()
-
-        context['tests'].append(e)
-
-        #members = inspect.getmembers(e)
-        #for m in members :
-        #    if isinstance(m[1], OutputParameter) :
-        #        out.append("Output parameter found: %s = %s" % (e.__class__.__name__ + '.' + m[0], m[1](e)))
+    context = {'tests': MeasuredTest.All.values() }
 
     return render(request, 'django-measure-test/index.html', context)
